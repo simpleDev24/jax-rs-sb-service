37eeab455f5e55e0888004754f207216b4caebe2
Kanad <kanadsante@gmail.com>
2021-11-03 17:14:44 +0530

    first commit

---
+++ src/main/java/com/simpledev/jaxrssbservice/JaxRsSbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.simpledev.jaxrssbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaxRsSbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaxRsSbServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simpledev/jaxrssbservice/JaxRsSbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpledev.jaxrssbservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaxRsSbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
